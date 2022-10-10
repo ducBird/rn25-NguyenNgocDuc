1ba141d8d02caee79157ded8a8fc97d16eac19cb
DucNguyen <nguyenngocduc94751@gmail.com>
2022-10-11 00:42:32 +0700

    upload new code

--- rn25duc-app/src/App.tsx
+++ rn25duc-app/src/App.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 // import logo from "./logo.svg";
 // import imgHouse from "./image/b";
-
+//
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
